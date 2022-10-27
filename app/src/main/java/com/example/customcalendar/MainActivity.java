9e11763b2f640f78f61bf93d7c088e0ca920bcf8
Ahmed Atia <aabdelshafy@dataserve.com.sa>
2022-10-27 12:22:24 +0200

    first commit

---
+++ app/src/main/java/com/example/customcalendar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.customcalendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import net.alhazmy13.hijridatepicker.date.hijri.HijriDatePickerDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        HijriDatePickerDialog h ;
+    }
+}
